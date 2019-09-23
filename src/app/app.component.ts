4bd851bb87cbe155251e64f67d593f05560a2d1b
benneee <benedictiknkeonye@gmail.com>
2019-09-23 05:50:05 +0100

    Created logout functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+import { AuthService } from './auth/auth.service';
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +15,17 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
-  onLogout() {}
+  onLogout() {
+    this.authService.logOut();
+    this.router.navigateByUrl('/auth');
+  }
 
   initializeApp() {
     this.platform.ready().then(() => {
