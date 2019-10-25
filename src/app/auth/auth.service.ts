263eca8192c7cb1f503a8b6f53b23770e77022bb
benneee <benedictiknkeonye@gmail.com>
2019-10-26 00:28:34 +0100

    Handling user input

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
-  private _userIsAuthenticated = false;
-  // private _userIsAuthenticated = true;
+  // private _userIsAuthenticated = false;
+  private _userIsAuthenticated = true;
 
   get userIsAuthenticated() {
     return this._userIsAuthenticated;
