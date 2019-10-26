8f071ffcc7a212de10dadea688cd315cfb16eaa8
benneee <benedictiknkeonye@gmail.com>
2019-10-26 14:16:43 +0100

    Updated all the receiving points with subscriptions

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -2,23 +2,25 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PlacesService } from './../../places.service';
 import { NavController } from '@ionic/angular';
 import { Place } from 'src/app/places/place.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-offer',
   templateUrl: './edit-offer.page.html',
   styleUrls: ['./edit-offer.page.scss']
 })
-export class EditOfferPage implements OnInit {
+export class EditOfferPage implements OnInit, OnDestroy {
+  place: Place;
+  editOfferForm: FormGroup;
+  private placeSub: Subscription;
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private navCtrl: NavController,
     private placesService: PlacesService
   ) {}
-  place: Place;
-  editOfferForm: FormGroup;
 
   ngOnInit() {
     this.route.paramMap.subscribe(paramMap => {
@@ -26,9 +28,13 @@ export class EditOfferPage implements OnInit {
         this.navCtrl.navigateBack('/places/tabs/offers');
         return;
       }
-      this.place = this.placesService.getPlace(paramMap.get('placeId'));
+      this.placeSub = this.placesService
+        .getPlace(paramMap.get('placeId'))
+        .subscribe(place => {
+          this.place = place;
+          this.editOffer();
+        });
       console.log('place: ', this.place);
-      this.editOffer();
     });
   }
 
@@ -49,5 +55,11 @@ export class EditOfferPage implements OnInit {
         validators: [Validators.maxLength(180)]
       })
     });
+  }
+
+  ngOnDestroy() {
+    if (this.placeSub) {
+      this.placeSub.unsubscribe();
+    }
   }
 }
