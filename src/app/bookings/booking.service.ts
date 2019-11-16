e652132a13efac7603e0fca4a903bd4a1421b4bf
benneee <benedictiknkeonye@gmail.com>
2019-11-16 14:40:12 +0100

    Added methods to add, delete and get bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,16 +1,29 @@
-import { AuthService } from './../auth/auth.service';
-import { Injectable } from '@angular/core';
-import { Booking } from './bookings.model';
-import { BehaviorSubject } from 'rxjs';
-import { take, delay, tap } from 'rxjs/operators';
+import { AuthService } from "./../auth/auth.service";
+import { Injectable } from "@angular/core";
+import { Booking } from "./bookings.model";
+import { BehaviorSubject } from "rxjs";
+import { take, delay, tap, switchMap, map } from "rxjs/operators";
+import { HttpClient } from "@angular/common/http";
 
+interface BookingData {
+  bookedFrom: string;
+  bookedTo: string;
+  firstName: string;
+  guestNumber: number;
+  lastName: string;
+  placeId: string;
+  placeImg: string;
+  placeTitle: string;
+  userId: string;
+}
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class BookingService {
+  firebaseUrl = "https://squat-ster.firebaseio.com/bookings.json";
   private _bookings = new BehaviorSubject<Booking[]>([]);
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   get bookings() {
     return this._bookings.asObservable();
@@ -26,6 +39,7 @@ export class BookingService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newBooking = new Booking(
       Math.random().toString(),
       placeId,
@@ -38,23 +52,66 @@ export class BookingService {
       dateFrom,
       dateTo
     );
-
-    return this.bookings.pipe(
-      take(1),
-      delay(2000),
-      tap(bookings => {
-        this._bookings.next(bookings.concat(newBooking));
-      })
-    );
+    return this.http
+      .post<{ name: string }>(this.firebaseUrl, { ...newBooking, id: null })
+      .pipe(
+        switchMap(res => {
+          generatedId = res.name;
+          return this.bookings;
+        }),
+        take(1),
+        tap(bookings => {
+          newBooking.id = generatedId;
+          this._bookings.next(bookings.concat(newBooking));
+        })
+      );
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(2000),
-      tap(bookings => {
-        this._bookings.next(bookings.filter(b => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(`https://squat-ster.firebaseio.com/bookings/${bookingId}.json`)
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap(bookings => {
+          this._bookings.next(bookings.filter(b => b.id !== bookingId));
+        })
+      );
+  }
+
+  fetchBookings() {
+    return this.http
+      .get<{ [key: string]: BookingData }>(
+        `${this.firebaseUrl}?orderBy="userId"&equalTo="${this.authService.userId}"`
+      )
+      .pipe(
+        map(bookingData => {
+          const bookings = [];
+          for (const key in bookingData) {
+            if (bookingData.hasOwnProperty(key)) {
+              bookings.push(
+                new Booking(
+                  key,
+                  bookingData[key].placeId,
+                  bookingData[key].userId,
+                  bookingData[key].placeTitle,
+                  bookingData[key].placeImg,
+                  bookingData[key].firstName,
+                  bookingData[key].lastName,
+                  bookingData[key].guestNumber,
+                  new Date(bookingData[key].bookedFrom),
+                  new Date(bookingData[key].bookedTo)
+                )
+              );
+            }
+          }
+          return bookings;
+        }),
+        tap(bookings => {
+          this._bookings.next(bookings);
+        })
+      );
   }
 }
